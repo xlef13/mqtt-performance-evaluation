a1262b7e627d2ea2541c0e73570c67e0a338a507
Felix Wagner <xlef13@arcor.de>
2020-05-25 17:45:59 +0200

    small changes to javascript + measurements made

---
+++ javascript/mqttPublishTime.js
@@ -0,0 +1,45 @@
+var mqtt = require('mqtt')
+const { PerformanceObserver, performance } = require('perf_hooks');
+const payloadModule = require('./payloads.js')
+
+// observer for measuring times
+const obs = new PerformanceObserver((list) => {
+  console.log(list.getEntries()[0].name + ";" + list.getEntries()[0].duration + ";" + list.getEntries()[0].startTime);
+});
+obs.observe({ entryTypes: ['function', 'measure'] });
+
+// configuration
+const host="localhost";
+const port=1883;
+const numberOfPublishes = 1000;
+const payload = payloadModule.big;
+// connect (blocking)
+performance.mark('connecting');
+var client  = mqtt.connect(host + ":" + port);
+performance.mark('connected');
+performance.measure('connecting to connected', 'connecting', 'connected');
+// client events
+client.on('connect', function () {
+  //start taking the actual measurements after connection is aknowledged
+
+})
+
+client.on('message', function (topic, message) {
+  console.log("published to received" + ";" + (now-lastPublishTime).toString() + ";" + now );
+})
+
+// subscribe containing time measurement
+function publish(){
+  client.publish('payload/empty', payload);
+}
+
+// Publish a number of times
+performance.mark('publishing');
+for(i=0;i<numberOfPublishes;i++){
+  publish();
+}
+performance.mark('published');
+performance.measure('publishing to published', 'publishing', 'published');
+// cleanup
+obs.disconnect();
+client.end()
--- javascript/mqttRoundTripTime.js
+++ javascript/mqttRoundTripTime.js
@@ -1,24 +1,31 @@
 const { PerformanceObserver, performance } = require('perf_hooks');
-
+var mqtt = require('mqtt')
+const payloadModule = require('./payloads.js')
+// observer for measuring times
 const obs = new PerformanceObserver((list) => {
   console.log(list.getEntries()[0].name + ";" + list.getEntries()[0].duration + ";" + list.getEntries()[0].startTime);
 });
 obs.observe({ entryTypes: ['function', 'measure'] });
 
-var mqtt = require('mqtt')
+// configuration
+const host="localhost"
+const port=1883
+const payload = payloadModule.big;
+
 performance.mark('connecting');
 var client  = mqtt.connect('mqtt://localhost:1883');
+performance.mark('connected');
+performance.measure('connecting to connected', 'connecting', 'connected');
+
 // client events
 client.on('connect', function () {
-  performance.mark('connected');
-  performance.measure('connecting to connected', 'connecting', 'connected');
-  takeMeasurements();
+
 })
 
 // This is a workaround as for some reasen performance.mark does not work in client.on('message')
 let lastPublishTime = 0;
 client.on('message', function (topic, message) {
-  let now = performance.now()
+  let now = performance.now();
   console.log("published to received" + ";" + (now-lastPublishTime).toString() + ";" + now );
 })
 
@@ -33,22 +40,18 @@ function subscribe(){
 // subscribe containing time measurement
 function publish(){
   performance.mark('publishing');
-  client.publish('payload/empty', '');
+
+  client.publish('payload/empty', payload);
   performance.mark('published');
   lastPublishTime = performance.now();
   performance.measure('publishing to published', 'publishing', 'published');
 }
 
-function takeMeasurements(){
-  // Subscribe to a single topic
-  subscribe();
+// Subscribe to a single topic and publish a msg
+subscribe();
+publish();
 
-  // Publish a number of times
-  for(i=0;i<10;i++){
-    publish();
-  }
 
-  // cleanup
-  obs.disconnect();
-  client.end()
-}
+// cleanup
+obs.disconnect();
+client.end()
